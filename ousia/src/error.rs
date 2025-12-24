fb00380321da9aea0eea1d3f289e3b3f872cc26a
Efedua Believe <efedua.bell@gmail.com>
2025-12-24 15:51:28 +0100

    Update Error: impl Display

--- ousia/src/error.rs
+++ ousia/src/error.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub enum Error {
     NotFound,
@@ -7,3 +9,18 @@ pub enum Error {
     Storage(String),
     TypeMismatch,
 }
+
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::NotFound => write!(f, "Not found"),
+            Error::Conflict => write!(f, "Conflict"),
+            Error::Serialize(err) => write!(f, "Serialization error: {}", err),
+            Error::Deserialize(err) => write!(f, "Deserialization error: {}", err),
+            Error::Storage(err) => write!(f, "Storage error: {}", err),
+            Error::TypeMismatch => write!(f, "Type mismatch"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
